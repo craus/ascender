f0ac9c3a8db812f0424a5c4484d46c627c1c7420
craus <craus902@gmail.com>
2017-02-19 23:14:22 +0400

    heroes can up skills

--- hero.js
+++ hero.js
@@ -12,6 +12,7 @@ hero = (params={}) => {
       intelligence: 1
     },
     level: 0,
+    skillPoints: 0,
     experience: 0,
     deselect: function() {
       this.selected = false
@@ -41,11 +42,25 @@ hero = (params={}) => {
       while (this.experience >= this.experienceToLevelUp()) {
         this.experience -= this.experienceToLevelUp()
         this.level += 1
+        this.skillPoints += 1
       }
+    },
+    skillUp: function(skill) {
+      if (this.skillPoints < 1) {
+        return
+      }
+      this.skillPoints -= 1
+      this.skills[skill] *= 1.2
     },
     paint: function() {
       setFormattedText(panel.find('.status'), this.status())
       setFormattedText(panel.find('.level'), this.level)
+      setFormattedText(panel.find('.skillPoints'), this.skillPoints)
+      panel.find('.skillPointsNotZero').toggle(this.skillPoints > 0)
+      panel.find('.speedUp').toggle(this.skillPoints > 0)
+      panel.find('.defenseUp').toggle(this.skillPoints > 0)
+      panel.find('.intelligenceUp').toggle(this.skillPoints > 0)
+      panel.find('.wealthUp').toggle(this.skillPoints > 0)
       setFormattedText(panel.find('.speed'), this.skills.speed)
       setFormattedText(panel.find('.defense'), this.skills.defense)
       setFormattedText(panel.find('.intelligence'), this.skills.intelligence)
@@ -66,6 +81,10 @@ hero = (params={}) => {
   setFormattedText(panel.find('.name'), hero.name)
   panel.find('.select').click(() => hero.select())
   panel.find('.abandon').click(() => hero.abandon())
+  panel.find('.speedUp').click(() => hero.skillUp('speed'))
+  panel.find('.defenseUp').click(() => hero.skillUp('defense'))
+  panel.find('.intelligenceUp').click(() => hero.skillUp('intelligence'))
+  panel.find('.wealthUp').click(() => hero.skillUp('wealth'))
 
   return hero
 }
